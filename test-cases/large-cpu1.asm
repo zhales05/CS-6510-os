dc0ce93c72e03d7011ac402374a039678d0f6272
Connor Fogg <connorstevenfogg@gmail.com>
2025-03-15 12:59:10 -0600

    Update large-cpu1.asm

--- test-cases/large-cpu1.asm
+++ test-cases/large-cpu1.asm
@@ -9,7 +9,7 @@ START   MVI R1 12   ; Load value 12 into R1
         DIV R9 R2 R3 ; R9 = R2 / R3
         ADD R10 R8 R9 ; R10 = R8 + R9
         SUB R11 R6 R7 ; R11 = R6 - R7
-        SWI 2       ; I/O operation (first of two)
+        SWI 4       ; I/O operation (first of two)
         MUL R12 R10 R5 ; R12 = R10 * R5
         DIV R13 R11 R5 ; R13 = R11 / R5
         ADD R14 R12 R13 ; R14 = R12 + R13
@@ -23,9 +23,8 @@ START   MVI R1 12   ; Load value 12 into R1
         ADD R22 R21 R2 ; R22 = R21 + R2
         SUB R23 R22 R1 ; R23 = R22 - R1
         MUL R24 R23 R4 ; R24 = R23 * R4
-        SWI 2       ; I/O operation (second of two)
+        SWI 4       ; I/O operation (second of two)
         DIV R25 R24 R5 ; R25 = R24 / R5
         ADD R26 R25 R3 ; R26 = R25 + R3
         SUB R27 R26 R4 ; R27 = R26 - R4
         MUL R0 R27 R1 ; R0 = R27 * R1
-        SWI 1       ; Terminate
