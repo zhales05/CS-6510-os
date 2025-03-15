8ea0f83c14028391bc677ba1b09dbe4c43a60fe6
Connor Fogg <connorstevenfogg@gmail.com>
2025-03-15 15:03:36 -0600

    Update small-io1.asm

--- test-cases/small-io1.asm
+++ test-cases/small-io1.asm
@@ -1,6 +1,6 @@
 START   MVI R1 5    ; Load value 5 into R1
         MVI R2 10   ; Load value 10 into R2
         ADD R0 R1 R2 ; R0 = R1 + R2
-        SWI 2       ; I/O operation
+        SWI 4       ; I/O operation
         SUB R0 R2 R1 ; R0 = R2 - R1
-        SWI 2       ; I/O operation
+        SWI 4       ; I/O operation
